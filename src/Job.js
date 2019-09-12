a53ffccf93f07f6d3e5ec96129e0cbe710b0d5fa
Artemiy <aaegorof@gmail.com>
2019-09-12 22:13:58 +0300

    add less Support

---
+++ src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Job = () => {
+  const job = "toy"
+  return (
+      <div>{job}</div>
+      )
+
+}
+
+export default Job
---
+++ src/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
